bd1693a05f8f237478f2b4fb83163bbbf69a6f40
rekren <ruchanekren@gmail.com>
2023-02-06 13:17:15 +0100

    initial commit

---
+++ helper_script.R
@@ -0,0 +1,7 @@
+library(datadrivencv)
+# This will create the template from the
+use_datadriven_cv(full_name = "Rüçhan Ekren",
+                  data_location ="https://docs.google.com/spreadsheets/d/1k5ZFHFsBo9saAWSZ7pssbOaB4Hw49Jt8URnl2vwmHf0",
+                  use_network_logo = F)
+# Now you need to modify will be generated "cv.rmd" template
+# for your own type of information and taste of aesthetics.
